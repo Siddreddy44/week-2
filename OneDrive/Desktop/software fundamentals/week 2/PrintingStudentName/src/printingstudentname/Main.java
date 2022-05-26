ec0eb6b4e320191ee9f51e3a1ba82a07475d45b1
radhi <radhi@10.48.35.32>
2022-05-26 09:42:04 -0400

    3rd weekk

--- OneDrive/Desktop/software fundamentals/week 2/PrintingStudentName/src/printingstudentname/Main.java
+++ OneDrive/Desktop/software fundamentals/week 2/PrintingStudentName/src/printingstudentname/Main.java
@@ -24,6 +24,7 @@ public class Main {
         
         for(int i =0; i<list.length; i++){
             System.out.println(list[i].getName() + list[i].getAge());
+            System.out.println(list[i].getName() + list[i].getAge());
             
         }
     }
