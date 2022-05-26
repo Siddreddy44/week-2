c6788a3bfafc2978532c50adc0f6bc0b50f32743
radhi <radhi@10.48.35.32>
2022-05-26 09:32:49 -0400

    3rd week

--- OneDrive/Desktop/software fundamentals/week 2/PrintingStudentName/src/printingstudentname/Main.java
+++ OneDrive/Desktop/software fundamentals/week 2/PrintingStudentName/src/printingstudentname/Main.java
@@ -9,13 +9,13 @@ public class Main {
         Student[] list = new Student[3];
         
         Student s1 = new Student();
-        s1.setName("Jhon");
+        s1.setName("Sharan");
         s1.setAge(19);
         Student s2 = new Student();
-        s2.setName("peter");
+        s2.setName("Perio");
         s2.setAge(20);
         Student s3 = new Student();
-        s3.setName("harman");
+        s3.setName("Ashwin");
         s3.setAge(21);
        
         list[0] = s1;
@@ -24,6 +24,7 @@ public class Main {
         
         for(int i =0; i<list.length; i++){
             System.out.println(list[i].getName() + list[i].getAge());
+            
         }
     }
     
